5d89ac47c5b0526b71910e51538a24cc2ad0e39c
Sai Chandhrasekhar <skchandhrasekhar@gmail.com>
2017-09-11 19:57:27 -0700

    fixed bugs in content script need to display properly

--- background.js
+++ background.js
@@ -2,10 +2,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, callback)
 {
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", request.url, true);
-	console.log("Requesting url:" + request.url);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) 
-			document.getElementById("h1").innerText = xhr.responseText;
+	xhr.onload = function() {
+		callback(xhr.responseText)
 	}
 	xhr.send();
 	return true;
--- content.js
+++ content.js
@@ -1,48 +1,43 @@
 // Saikrishna Chandhrasekhar
 // September 8, 2017
 
-function getIP()
-{
-	chrome.runtime.sendMessage({url: "ipinfo.io"}, function(responseText) {
-		var temp = document.createElement("div");
-		temp.innerHTML = responseText;
-		var address = temp.getElementsByTagName("h1")[0].innerText;
-		getCompanyName(address);
-	});
-}
+chrome.runtime.sendMessage({url: "https://ipinfo.io/"}, function(responseText) {
+	var temp = document.createElement("div");
+	temp.innerHTML = responseText;
+	var address = temp.getElementsByTagName("h1")[0].innerText;
+	getCompanyName(address);
+});
+
 
 function getCompanyName(address)
 {
 	$(document).ready(function() {
-		$("div").mouseenter(function() {
-			var companyid = this.getElementsById("h4")[0];
-			var companyName = companyid.innerText;
-			getData(address, companyName);
+		$(".job-card-search__title-line").each(function() {
+			
 		});
 	});
 }
 
 function getData(address, companyName)
 {
-	chrome.runtime.sendMessage({url: "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=177556&t.k=kvtUQd0ashS&userip=" + address 
-	+ "&actions=employers&q=" + companyName + "&userAgent=" + navigator.userAgent}, function(responseText) {
+	var searchURL = "http://api.glassdoor.com/api/api.htm?t.p=194337&t.k=kenIlW2Q28g&format=json&v=1&action=employers&q=" + companyName + "&userip=" + address;
+	chrome.runtime.sendMessage({url: searchURL}, function(responseText) {
 		var response = JSON.parse(responseText || null);
 		if(response != null)
 		{
-			if(response["status"] == true)
+			if(response["success"] == true)
 			{
 				var companyRating = response["response"].employers[0].overallRating;
-				displayRating(companyName, companyRating);
+				console.log(companyRating);
+				displayRating(companyRating);
 			}
 		}
 	});
 }
 
-function displayRating(companyName, companyRating)
+function displayRating(companyRating)
 {
-	var display = this.createElement("div");
-	var rating = this.createTextNode(companyRating);
-	display.append(rating);
-	display.append("<a href='https://www.glassdoor.com/index.htm'>powered by <img src='https://www.glassdoor.com/static/img/api/glassdoor_logo_80.png' title='Job Search' /></a>");
-	this.append(display);
+	
+	var test = document.getElementById("gd_rating");
+	console.log(test);
 }
