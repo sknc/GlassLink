b019e65f5f5011402a77e1159d534ad2149dd359
Sai Chandhrasekhar <skchandhrasekhar@gmail.com>
2017-09-14 16:55:30 -0700

    changed some jquery

--- content.js
+++ content.js
@@ -12,13 +12,17 @@ chrome.runtime.sendMessage({url: "https://ipinfo.io/"}, function(responseText) {
 function getCompanyName(address)
 {
 	$(document).ready(function() {
-		$(".job-card-search__title-line").each(function() {
-			
+		$(".job-card-search__company-location-wrapper").hover(function() {
+			var companyName = this.getElementsByTagName("h4")[0].innerText;
+			var ratingDiv = document.createElement("div");
+			getData(address, companyName, ratingDiv);
+			$(this).append(ratingDiv);
+			$(this).append("<a href='https://www.glassdoor.com/index.htm'>powered by <img src='https://www.glassdoor.com/static/img/api/glassdoor_logo_80.png' title='Job Search' /></a>");
 		});
 	});
 }
 
-function getData(address, companyName)
+function getData(address, companyName, ratingDiv)
 {
 	var searchURL = "http://api.glassdoor.com/api/api.htm?t.p=194337&t.k=kenIlW2Q28g&format=json&v=1&action=employers&q=" + companyName + "&userip=" + address;
 	chrome.runtime.sendMessage({url: searchURL}, function(responseText) {
@@ -28,16 +32,8 @@ function getData(address, companyName)
 			if(response["success"] == true)
 			{
 				var companyRating = response["response"].employers[0].overallRating;
-				console.log(companyRating);
-				displayRating(companyRating);
+				ratingDiv.innerHTML = "<span id = glassdoor_rating>" + companyRating + "</span>";
 			}
 		}
 	});
-}
-
-function displayRating(companyRating)
-{
-	
-	var test = document.getElementById("gd_rating");
-	console.log(test);
 }
